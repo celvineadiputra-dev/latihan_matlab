3cf69cf8bcd20aab9d31f33705d08afa6c140b66
celvineadiputra-dev <celvineadiputra2@gmail.com>
2021-01-27 20:18:55 +0700

    Update Content

--- ImageTOBiner.m
+++ ImageTOBiner.m
@@ -3,17 +3,62 @@ clear all;
 close all;
 
 I = imread('Img/github.png');
+% imcrop(I);
 % I = rgb2gray(I);
 % 
 A = zeros(size(I,1), size(I,2));
 
 for i=1:size(A,1)
     for j=1:size(A,2)
-        if(sum(I(i,j)) > 240)
+        if(I(i,j) > 240)
             A(i,j) = 1;
         end
     end
 end
+X = 2;
+Y = 3;
 
+figure("Name", "Biner", "NumberTitle", "off")
+subplot(Y,X, 1)
+imshow(im2bw(I)); hold on;
+imshow(imresize(imcrop(I,[304.5 64.5 601 507]), 0.5));
+title("Biner Function")
 
+subplot(Y,X, 2)
 imshow(A)
+title("Biner Manual")
+
+subplot(Y,X,3)
+imshow(~im2bw(I))
+title('Biner Reverse Biner Function')
+
+subplot(Y,X,4)
+imshow(~A)
+title('Biner Reverse Biner Manual')
+
+subplot(Y,X,5)
+B = im2bw(I);
+for i=1:size(B, 1)
+    for j = 1:size(B, 2)
+        if(B(i,j) == 0)
+            B(i,j) = 1;
+        else
+            B(i,j) = 0;
+        end
+    end
+end
+imshow(B)
+title('Biner Reverse Manual Biner Function')
+
+subplot(Y,X,6)
+for i=1:size(A, 1)
+    for j = 1:size(A, 2)
+        if(A(i,j) == 0)
+            A(i,j) = 1;
+        else
+            A(i,j) = 0;
+        end
+    end
+end
+imshow(A)
+title('Biner Reverse Manual Biner Manual')
--- integralImageM.m
+++ integralImageM.m
@@ -1,15 +1,15 @@
 I = [2 1 0 0;0 1 2 0; 1 2 1 0; 1 1 0 2];
 J = integralImage(I)
 
-% A = imread('cameraman.tif');
-% A = zeros(100,100);
-% A(50,50) = 156;
-% A(30,30) = 1;
-% 
-% % A = [1 1 1; 1 2 1; 1 1 1];
-% % A = [7 4 0 1; 5 6 2 2; 6 10 7 8; 1 4 2 0];
-% A = [1 2 3; 2 1 3];
-% sum(sum(A))
-% 
-% localMean = imboxfilt(A,3);
-% imshowpair(A,localMean,'montage')
+A = imread('cameraman.tif');
+A = zeros(100,100);
+A(50,50) = 156;
+A(30,30) = 1;
+
+% A = [1 1 1; 1 2 1; 1 1 1];
+% A = [7 4 0 1; 5 6 2 2; 6 10 7 8; 1 4 2 0];
+A = [1 2 3; 2 1 3];
+sum(sum(A))
+
+localMean = imboxfilt(A,3);
+imshowpair(A,localMean,'montage')
